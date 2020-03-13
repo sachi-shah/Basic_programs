dbba2053612dab25d2650a0b60b0b3ddb7d636b1
sachi-shah <56812583+sachi-shah@users.noreply.github.com>
2020-03-13 16:02:36 +0530

    Add files via upload

---
+++ method_overloading.py
@@ -0,0 +1,19 @@
+# python program showing method overloading
+
+
+class Human:
+    def sayHello(self, name=None):
+        if name is not None:
+            print('Hello ' + name)
+        else:
+            print('Hello')
+
+
+# create instance
+obj = Human()
+
+# call the method
+obj.sayHello()
+
+# call the method with a parameter
+obj.sayHello('everyone')
---
+++ method_overriding.py
@@ -0,0 +1,19 @@
+# program showing method overriding
+
+
+class Employee:
+    def add(self, salary, incentive):
+        print('Total salary in base class = ', salary + incentive)
+
+
+class Department(Employee):
+    temp = "I am member of department class"
+
+    def add(self, salary, incentive):
+        print(self.temp)
+        print('Total salary in derived class = ', salary + incentive)
+        super(Department, self).add(salary, incentive)
+
+
+dept = Department()
+dept.add(45000, 5000)
